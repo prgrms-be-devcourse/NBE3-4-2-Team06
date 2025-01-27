534f81d006bd22501735b6ee0d3dd116f1b40d28
Hoon <xianse10@gmail.net>
2025-01-27 11:22:07 +0900

    chore: 프로젝트 초기 디렉토리 구조 설정 및 .gitignore에 application.properties 제외 추가

---
+++ src/main/java/Funding/Startreum/maincontroller.java
@@ -0,0 +1,16 @@
+package Funding.Startreum;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class maincontroller {
+
+    @GetMapping("/")
+    public String maintest() {
+
+
+
+        return "main";
+    }
+}
