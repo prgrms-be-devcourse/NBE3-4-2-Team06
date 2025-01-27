771d11af25df3d78b16e177728c9c9ad17942d4c
Hoon <xianse10@gmail.net>
2025-01-27 12:09:01 +0900

    feat: 프로젝트 각 도메인의 엔티티 클래스 생성

---
+++ src/main/java/Funding/Startreum/domain/transaction/Transaction.java
@@ -0,0 +1,52 @@
+package Funding.Startreum.domain.transaction;
+
+import Funding.Startreum.domain.users.User;
+import Funding.Startreum.domain.funding.Funding;
+import Funding.Startreum.domain.virtualaccount.VirtualAccount;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer transactionId; // 거래 고유 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "funding_id")
+    private Funding funding; // 펀딩 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_id")
+    private User admin; // 관리자 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_account_id", nullable = false)
+    private VirtualAccount senderAccount; // 송신 계좌
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_account_id", nullable = false)
+    private VirtualAccount receiverAccount; // 수신 계좌
+
+    private BigDecimal amount; // 거래 금액
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type; // 거래 유형
+
+    private LocalDateTime transactionDate; // 거래 일자
+
+    public enum TransactionType {
+        REMITTANCE,  // 송금
+        REFUND       // 환불
+    }
+}
