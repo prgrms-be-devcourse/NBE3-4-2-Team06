a5910f5f27126f715a324f598caf7c6e646eaaef
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-01-31 15:15:04 +0900

    feat: 프로젝트 생성 기능 개발

--- src/main/java/Funding/Startreum/domain/project/controller/ProjectController.java
+++ src/main/java/Funding/Startreum/domain/project/controller/ProjectController.java
@@ -1,7 +1,27 @@
 package Funding.Startreum.domain.project.controller;
 
-import org.springframework.stereotype.Controller;
+import Funding.Startreum.domain.project.dto.ProjectCreateRequestDto;
+import Funding.Startreum.domain.project.dto.ProjectCreateResponseDto;
+import Funding.Startreum.domain.project.entity.Project;
+import Funding.Startreum.domain.project.service.ProjectService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @PostMapping("/api/beneficiary/create")
+    public ResponseEntity<?> createProject(@AuthenticationPrincipal Integer userId, @Valid @RequestBody ProjectCreateRequestDto projectCreateRequestDto) {
+        ProjectCreateResponseDto pcrd = projectService.createProject(userId, projectCreateRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
 }
--- src/main/java/Funding/Startreum/domain/project/service/ProjectService.java
+++ src/main/java/Funding/Startreum/domain/project/service/ProjectService.java
@@ -1,7 +1,52 @@
 package Funding.Startreum.domain.project.service;
 
+import Funding.Startreum.domain.project.dto.ProjectCreateRequestDto;
+import Funding.Startreum.domain.project.dto.ProjectCreateResponseDto;
+import Funding.Startreum.domain.project.entity.Project;
+import Funding.Startreum.domain.project.repository.ProjectRepository;
+import Funding.Startreum.domain.users.User;
+import Funding.Startreum.domain.users.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.net.ResponseCache;
+import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public ProjectCreateResponseDto createProject(Integer userId, ProjectCreateRequestDto projectCreateRequestDto) {
+        //사용자 검증
+        User user = userRepository.findById(userId).orElseThrow(() ->
+        new ResponseStatusException(HttpStatus.UNAUTHORIZED, "사용자를 찾을 수 없습니다"));    //사용자를 찾을 수 없을 시 401 에러
+
+        //프로젝트 생성
+        Project project = new Project();
+        project.setCreator(user);
+        project.setTitle(projectCreateRequestDto.title());
+        project.setBannerUrl(projectCreateRequestDto.bannerUrl());
+        project.setDescription(projectCreateRequestDto.description());
+        project.setFundingGoal(projectCreateRequestDto.fundingGoal());
+        project.setCurrentFunding(BigDecimal.ZERO);
+        project.setStartDate(projectCreateRequestDto.startDate());
+        project.setEndDate(projectCreateRequestDto.endDate());
+        project.setStatus(Project.Status.ONGOING);
+        project.setIsApproved(Project.ApprovalStatus.AWAITING_APPROVAL);
+        project.setCreatedAt(LocalDateTime.now());
+        project.setUpdatedAt(LocalDateTime.now());
+
+        projectRepository.save(project);
+
+        ProjectCreateResponseDto projectCreateResponseDto = new ProjectCreateResponseDto(project.getProjectId(), project.getTitle(), project.getCreatedAt());
+        return projectCreateResponseDto;
+    }
 }
