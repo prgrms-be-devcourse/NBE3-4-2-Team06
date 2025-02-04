c191eab283212045dbe7f09d789e64ac1eb78fa2
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-02-04 15:46:40 +0900

    feat: 접근 권한 추가 및 파라미터 못 읽는 오류 수정

--- src/main/java/Funding/Startreum/domain/project/controller/ProjectController.java
+++ src/main/java/Funding/Startreum/domain/project/controller/ProjectController.java
@@ -4,6 +4,7 @@ import Funding.Startreum.domain.project.dto.ProjectApprovalResponseDto;
 import Funding.Startreum.domain.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +15,8 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping("/requestApprove/{projectId}")
-    public ResponseEntity<ProjectApprovalResponseDto> requestApprove(@PathVariable Integer projectId, @RequestHeader("Authorization") String token) {
+    @PreAuthorize("hasRole('BENEFICIARY')")
+    public ResponseEntity<ProjectApprovalResponseDto> requestApprove(@PathVariable("projectId") Integer projectId, @RequestHeader("Authorization") String token) {
         ProjectApprovalResponseDto responseDto = projectService.requestApprove(projectId, token);
 
         return ResponseEntity.ok(responseDto);
