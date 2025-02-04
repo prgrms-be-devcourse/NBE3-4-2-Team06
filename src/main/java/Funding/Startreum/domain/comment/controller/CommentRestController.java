cb864e3c265bd4b2a19a040fd71c37e4fbf80aa5
han <tkrhktkdwk10@gmail.com>
2025-02-04 10:18:01 +0900

    feat(comment): apply changes from merge

--- src/main/java/Funding/Startreum/domain/comment/controller/CommentRestController.java
+++ src/main/java/Funding/Startreum/domain/comment/controller/CommentRestController.java
@@ -1,13 +1,127 @@
 package Funding.Startreum.domain.comment.controller;
 
+import Funding.Startreum.domain.comment.dto.request.CommentRequest;
+import Funding.Startreum.domain.comment.dto.response.CommentResponse;
 import Funding.Startreum.domain.comment.service.CommentService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/comment")
 @RequiredArgsConstructor
 public class CommentRestController {
-    private final CommentService commentService;
+
+    private final CommentService service;
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getComment(
+            @PathVariable("projectId") int projectId) {
+
+        List<CommentResponse> comments = service.getComment(projectId);
+
+        if (comments.isEmpty()) {
+            return ResponseEntity.ok(
+                    Map.of("status", "success",
+                            "message", "댓글이 없습니다.",
+                            "data", comments
+                    )
+            );
+        } else {
+            return ResponseEntity.ok(
+                    Map.of("status", "success",
+                            "message", "댓글 조회에 성공했습니다.",
+                            "data", comments
+                    )
+            );
+        }
+
+    }
+
+    @PostMapping("/{projectId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> createComment(
+            @PathVariable("projectId") int projectId,
+            @Valid @RequestBody CommentRequest request,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        CommentResponse response = service.createComment(projectId, request, userDetails.getUsername());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        Map.of(
+                                "status", "success",
+                                "message", "댓글 생성에 성공했습니다.",
+                                "data", response
+                        )
+                );
+    }
+
+    @PutMapping("/{commentId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> modifyComment(
+            @PathVariable("commentId") int commentId,
+            @RequestBody @Valid CommentRequest request,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        try {
+            CommentResponse response = service.modifyComment(request, commentId, userDetails.getUsername());
+            return ResponseEntity.ok(
+                    Map.of(
+                            "status", "success",
+                            "message", "댓글 에 성공했습니다.",
+                            "data", response
+                    )
+            );
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(
+                            Map.of(
+                                    "status", "error",
+                                    "message", "댓글을 찾을 수 없습니다.",
+                                    "code", HttpStatus.NOT_FOUND.value()
+                            )
+                    );
+        }
+    }
+
+    @DeleteMapping("/{commentId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> deleteComment(
+            @PathVariable("commentId") int commentId,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        try {
+            service.deleteComment(commentId, userDetails.getUsername());
+            return ResponseEntity.ok(
+                    Map.of(
+                            "status", "success",
+                            "message", "댓글 삭제에 성공했습니다.",
+                            "code", HttpStatus.OK.value()
+                    )
+            );
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(
+                            Map.of(
+                                    "status", "error",
+                                    "message", "댓글을 찾을 수 없습니다.",
+                                    "code", HttpStatus.NOT_FOUND.value()
+                            )
+                    );
+        }
+    }
+
 }
--- src/main/java/Funding/Startreum/domain/comment/service/CommentService.java
+++ src/main/java/Funding/Startreum/domain/comment/service/CommentService.java
@@ -1,13 +1,96 @@
 package Funding.Startreum.domain.comment.service;
 
+import Funding.Startreum.domain.comment.dto.request.CommentRequest;
+import Funding.Startreum.domain.comment.dto.response.CommentResponse;
+import Funding.Startreum.domain.comment.entity.Comment;
 import Funding.Startreum.domain.comment.repository.CommentRepository;
+import Funding.Startreum.domain.project.Project;
+import Funding.Startreum.domain.project.ProjectRepository;
+import Funding.Startreum.domain.users.User;
+import Funding.Startreum.domain.users.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
-    final private CommentRepository commentRepository;
+    final private CommentRepository repository;
+    final private UserRepository userRepository;
+    final private ProjectRepository projectRepository;
+
+    @Transactional(readOnly = true)
+    public List<CommentResponse> getComment(int projectId) {
+        List<Comment> comments = repository.findByProject_ProjectId(projectId);
+
+        return comments.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentResponse createComment(int projectId, CommentRequest request, String username) {
+        Comment comment = new Comment();
+
+        User user = userRepository.findByName(username)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다 : " + username));
+
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다 : " + projectId));
+
+        comment.setProject(project);
+        comment.setUser(user);
+        comment.setContent(request.content());
+        comment.setCreatedAt(LocalDateTime.now());
+        comment.setUpdatedAt(LocalDateTime.now());
+        repository.save(comment);
+
+        return mapToDto(comment);
+    }
+
+    @Transactional
+    public CommentResponse modifyComment(CommentRequest request, int commentId, String username) {
+        Comment comment = repository.findByCommentId(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다 : " + commentId));
+
+        if (!comment.getUser().getName().equals(username)) {
+            throw new AccessDeniedException("댓글 수정 권한이 없습니다.");
+        }
+
+        comment.setContent(request.content());
+        repository.save(comment);
+
+        return mapToDto(comment);
+    }
+
+    @Transactional
+    public void deleteComment(int commentId, String username) {
+        Comment comment = repository.findByCommentId(commentId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글을 찾을 수 없습니다 : " + commentId));
+
+        if (!comment.getUser().getName().equals(username)) {
+            throw new AccessDeniedException("댓글 삭제 권한이 없습니다.");
+        }
+
+        repository.delete(comment);
+    }
+
+    private CommentResponse mapToDto(Comment comment) {
+        return new CommentResponse(
+                comment.getCommentId(),
+                comment.getProject().getProjectId(),
+                comment.getUser().getUserId(),
+                comment.getContent(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt()
+        );
+    }
 
 }
