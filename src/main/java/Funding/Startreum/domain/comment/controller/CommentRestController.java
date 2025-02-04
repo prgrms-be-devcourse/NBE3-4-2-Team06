cbb67d95ef9e827fa3c6ef1ca2e276067431e6c9
한상훈 <83763597+HSH02@users.noreply.github.com>
2025-02-04 09:37:14 +0900

    feat: comment-create -> comment 병합 (#19)
    * 123
    
    * feat(comment) : add comment create feature

--- src/main/java/Funding/Startreum/domain/comment/controller/CommentRestController.java
+++ src/main/java/Funding/Startreum/domain/comment/controller/CommentRestController.java
@@ -1,13 +1,43 @@
 package Funding.Startreum.domain.comment.controller;
 
+import Funding.Startreum.domain.comment.dto.request.CommentCreateRequest;
+import Funding.Startreum.domain.comment.dto.response.CommentResponse;
 import Funding.Startreum.domain.comment.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/comment")
 @RequiredArgsConstructor
 public class CommentRestController {
-    private final CommentService commentService;
+    private final CommentService service;
+
+    @PostMapping("/{projectId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> createComment(
+            @PathVariable("projectId") int projectId,
+            @Valid @RequestBody CommentCreateRequest request,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        CommentResponse response = service.createComment(projectId, request, userDetails.getUsername());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                    Map.of("status", "success",
+                        "message", "댓글 생성에 성공했습니다.",
+                        "data", response
+                )
+        );
+    }
+
 }
--- src/main/java/Funding/Startreum/domain/comment/service/CommentService.java
+++ src/main/java/Funding/Startreum/domain/comment/service/CommentService.java
@@ -1,13 +1,56 @@
 package Funding.Startreum.domain.comment.service;
 
+import Funding.Startreum.domain.comment.dto.request.CommentCreateRequest;
+import Funding.Startreum.domain.comment.dto.response.CommentResponse;
+import Funding.Startreum.domain.comment.entity.Comment;
 import Funding.Startreum.domain.comment.repository.CommentRepository;
+import Funding.Startreum.domain.project.Project;
+import Funding.Startreum.domain.project.ProjectRepository;
+import Funding.Startreum.domain.users.User;
+import Funding.Startreum.domain.users.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
-    final private CommentRepository commentRepository;
+    final private CommentRepository repository;
+    final private UserRepository userRepository;
+    final private ProjectRepository projectRepository;
 
+    @Transactional
+    public CommentResponse createComment(int projectId, CommentCreateRequest request, String username) {
+        Comment comment = new Comment();
+
+        User user = userRepository.findByName(username)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다 : " + username));
+
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다 : " + projectId));
+
+        comment.setProject(project);
+        comment.setUser(user);
+        comment.setContent(request.content());
+        comment.setCreatedAt(LocalDateTime.now());
+        comment.setUpdatedAt(LocalDateTime.now());
+
+        repository.save(comment);
+
+        return mapToDto(comment);
+    }
+
+    private CommentResponse mapToDto(Comment comment) {
+        return new CommentResponse(
+                comment.getCommentId(),
+                comment.getProject().getProjectId(),
+                comment.getUser().getUserId(),
+                comment.getContent(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt()
+        );
+    }
 }
