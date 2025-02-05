0ace60ea6550585b16d41194e7a5baf044a0a7a0
Hoon <xianse10@gmail.net>
2025-02-05 15:42:00 +0900

    feat : 관리자 ì²페이지 생성 및 프로젝트 관리 페이지 생성

--- src/main/java/Funding/Startreum/domain/project/ProjectRepository.java
+++ src/main/java/Funding/Startreum/domain/project/ProjectRepository.java
@@ -3,6 +3,9 @@ package Funding.Startreum.domain.project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    List<Project> findByIsApproved(Project.ApprovalStatus approvalStatus);
 }
